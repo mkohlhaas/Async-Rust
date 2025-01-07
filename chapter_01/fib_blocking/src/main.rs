39e547ea9ceaf469ac0e5cfa9e5dcd486acd32bd
Hans-J. Schmid <knock@myopendoor.de>
2025-01-07 18:17:38 +0100

    prep ch01

--- chapter_01/fib_blocking/src/main.rs
+++ chapter_01/fib_blocking/src/main.rs
@@ -1,22 +1,23 @@
 use std::thread;
 
 fn fibonacci(n: u64) -> u64 {
-  if n == 0 || n == 1 {
-    return n;
+  match n {
+    0 | 1 => n,
+    _ => fibonacci(n - 1) + fibonacci(n - 2),
   }
-  fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 fn main() {
   let mut threads = Vec::new();
 
-  for i in 0..8 {
+  for i in 0..4 {
     let handle = thread::spawn(move || {
       let result = fibonacci(4000);
       println!("Thread {} result: {}", i, result);
     });
     threads.push(handle);
   }
+
   for handle in threads {
     handle.join().unwrap();
   }
